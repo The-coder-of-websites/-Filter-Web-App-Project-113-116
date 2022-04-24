fb75112d5caca55312e73690b64c480239c3b348
The-coder-of-websites <89913566+The-coder-of-websites@users.noreply.github.com>
2022-04-24 20:25:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup(){
+    canvas=createCanvas(500,400);
+    canvas.center();
+}
+
+function take_snapshot(){
+    save("my_filtered_image.png");
+}
+
+function preload(){
+
+}
+
+function draw(){
+    
+}
