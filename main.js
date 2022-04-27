795574cdda371d6f1f46bd40045ffc773dc49574
The-coder-of-websites <89913566+The-coder-of-websites@users.noreply.github.com>
2022-04-27 15:53:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
+
 function setup(){
     canvas=createCanvas(500,400);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(500,400);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log('Posenet is loaded');
+}
+
+function draw(){
+    image(video,0,0,500,400);
+
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("nose x= "+results[0].pose.nose.x);
+        console.log("nose y= "+results[0].pose.nose.y);
+    }
 }
 
 function take_snapshot(){
     save("my_filtered_image.png");
-}
-
-function preload(){
-
-}
-
-function draw(){
-    
 }
